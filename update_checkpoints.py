a6343f299789e1c93854778658990ec27401fe12
Radu Apsan <radu.apsan@gmail.com>
2025-06-11 16:44:23 +0200

    Add arguments to update_checkpoints and explain program

--- update_checkpoints.py
+++ update_checkpoints.py
@@ -1,11 +1,6 @@
+import argparse
 import json
 import os
-import re
-
-IN_FILE = './prompts_plan_FULL_30_REP_res_test.json'
-CHECKPOINT_DIR = './checkpoints'
-OUT_FILE = './prompts_plan_FULL_30_REP_res_test_updated.json'
-
 
 def load_json_data(json_path):
     with open(json_path, 'r') as f:
@@ -29,9 +24,19 @@ def parse_checkpoint_file(filename):
 
 
 if __name__=='__main__':
-    with open(IN_FILE, 'r') as f:
-        data = json.load(f)
+    parser = argparse.ArgumentParser(prog="Checkpoint Updater", description="This program can be used in the event that main.py or parallel_main.py do not write the output file.\n\
+                                     It uses the input JSON file (-i) to determine the structure of what has to be added from the checkpoints folder (-c).\n \
+                                     The resulting data structure is written to the output JSON file (-o)\n")
+    parser.add_argument("-i", "--input_path", type=str, required=True, help="Path to input JSON file.")
+    parser.add_argument("-c", "--checkpoints_folder", type=str, required=True, help="Path to the checkpoint folder where the checkpoint files are saved.")
+    parser.add_argument("-o", "--output_path", type=str, required=True, help="Path to output JSON file.")
+    args = parser.parse_args()
 
+    IN_FILE = args.input_path
+    CHECKPOINT_DIR = args.checkpoints_path
+    OUT_FILE = args.output_path
+
+    data = load_json_data(IN_FILE)
     cp_files = os.listdir(CHECKPOINT_DIR)
         
     for i, llms in enumerate(data["prompts"]):
@@ -49,5 +54,6 @@ if __name__=='__main__':
                     task_rep = task_reps["repetition_id"]
                     if os.path.exists(f'{CHECKPOINT_DIR}/{llm}_temp_{temperature}_rep_{task_rep}_task.json'):
                         data["prompts"][i]["prompts"][j]["tasks"][k] = load_json_data(f'{CHECKPOINT_DIR}/{llm}_temp_{temperature}_rep_{task_rep}_task.json')
+
     with open(OUT_FILE, 'w') as out:
         json.dump(data, out, indent=4)
